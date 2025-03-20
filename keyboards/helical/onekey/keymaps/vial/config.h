9074bdd056f5e83a68a49f68465b82e550b6d7dc
Helical <0811helical@gmail.com>
2025-03-20 22:54:28 +0900

    [Add]Dockerビルドできる初期状態

---
+++ keyboards/helical/onekey/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xFF, 0x12, 0x5F, 0x7C, 0x43, 0xC6, 0xFA, 0x96}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 0}
